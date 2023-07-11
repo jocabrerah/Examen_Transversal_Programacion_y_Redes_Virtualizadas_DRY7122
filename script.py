1ff533e57bb146f68e7b5dc7902d5120fd0a8423
root <root@labvm.vm>
2023-07-11 02:56:32 +0000

    Agregado script.py con lista de nombres y apellidos

---
+++ script.py
@@ -0,0 +1,8 @@
+nombres_apellidos = ["Jorge Cabrera", "Juan Vega", "Luis Bahamondes", "Daniel Caicea"]
+
+def imprimir_lista(nombres):
+    for nombre in nombres:
+        print(nombre)
+
+imprimir_lista(nombres_apellidos)
+
